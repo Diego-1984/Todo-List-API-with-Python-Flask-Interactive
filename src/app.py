6ba059f1c72875783818dde0ee6357e9bcaaaafc
Diego-1984 <diegomenoruceta@gmail.com>
2023-04-04 11:57:22 +0000

    Proyecto terminado

--- src/app.py
+++ src/app.py
@@ -1,5 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
+
+todos = [{"label": "My first task", "done": False}]
+
+# Rutas
 
 @app.route("/home")  
 def home():  
@@ -12,6 +17,29 @@ def about_me():
 @app.route("/contact-me")  
 def contact_me():  
     return "Mi correo electrónico es diegomenoruceta@gmail.com" 
+    
+@app.route("/todos", methods=['GET'])
+def get_todos():  
+    return jsonify(todos)
+
+import json
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    print("Incoming request with the following body", request_body)
+    new_todo = json.loads(request_body)
+    todos.append(new_todo)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.pop(position)
+    return jsonify(todos)
 
 
-app.run(host='0.0.0.0', port=4201)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
+
+print(request.data)
